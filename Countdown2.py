7768b19b13a01842d05e19d776347173839f6cfc
Karen-Gil <Karen.gil@ms223.org>
2016-03-29 19:23:31 -0400

    Update Countdown2.py

--- Countdown2.py
+++ Countdown2.py
@@ -1,6 +1,11 @@
 countdown = input('pick a number')
 number= countdown
 while number>0:
+    #number has to be grater than zero in order for it to countdown duh!!!
     print number
     number= number-1
+    #In order for there to be a countdown it has to be number-1
 print('BOOM!!!')
+# I origanaly wrote print boom as showen below but it printed boom after every number it counted down so I didnt put any white 
+#space that fixed my code 
+    #print ('boom')
