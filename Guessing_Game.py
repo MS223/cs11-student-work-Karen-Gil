79c2b845d2c0f4f55f853e3a6deb4e9a477a67d2
Karen-Gil <Karen.gil@ms223.org>
2016-04-05 09:13:08 -0400

    Update Guessing_Game.py

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,14 +1,17 @@
 range= input('Pick a range')
 guess= input('Okay now pick a number in that range')
-
+score=0
 import random
 random= (random.randint(1,range))
 while guess != random:
     if guess < random:
         print('Guess higher')
+        score= score+1
         guess= input('Guess again pick a number')
     elif guess > random:
         print('Guess lower')
+        score= score+1
         guess= input('guess again pick a number')
-#elif  guess == random
 print("YAYYYYYYYY YOU GOT IT RIGHT!!!!!!!!!")
+print "It took you " + str(score) + " try to get it right"
+#elif  guess == random
