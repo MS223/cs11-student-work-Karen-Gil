46660223b29be3e9ecebe0ef72c0541ccfa84a63
Karen-Gil <Karen.gil@ms223.org>
2016-04-08 08:42:06 -0400

    Create Rock_Paper_Scissor.py

---
+++ Rock_Paper_Scissor.py
@@ -0,0 +1,10 @@
+print "Lets play Rock Paper Scissors"
+user_choice=raw_input("Pick now")
+move=["rock","paper","scissors"]
+computer_move= (random.randint(move)
+
+if user_choice== rock and computer_move== scissors
+or 
+user_choice== paper and computer_move== rock
+or 
+user_choice== scissors and computer_move== paper
