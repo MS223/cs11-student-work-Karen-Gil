5fec634959c91c6538cd31cd0cbae0c6b5736a3c
Karen-Gil <Karen.gil@ms223.org>
2016-04-15 08:42:54 -0400

    Update War.py

--- War.py
+++ War.py
@@ -1,22 +1,32 @@
+def player_turn(player):
+    card  = deck.pop()
+    print player + "drew" + str(card)
+    return str(card)
+
 import random
 def shuffled_deck():
-	basic_deck = range(2, 52)
-	random.shuffle(basic_deck)
-	return basic_deck
+	deck = range(2, 52)
+	random.shuffle(deck)
+	return deck
+
+deck=[]
+hand= shuffled_deck()
+deck.extend(hand)
 
 player1= raw_input("What's your name:")
 player2=raw_input("What's your name:")
 
-Player#start here
-def player_turn (player):
-    card= deck.pop()
-    print player1 + "drew" + card
-    return str(card)
+player1_score=0
+player2_score= 0
+
 
 player_turn(player1)
 player_turn(player2)
 
-if player1 > player2:
-    player1_score +1
-elif player2 > player1:
-    player2_score +1
+while deck:
+    if player_turn(player1) > player_turn(player2):
+    player1_score = player1_score +1
+    print player1_score
+elif player_turn(player2) > player_turn(player1):
+    player2_score=player2_score+1
+    print(player2_score)
