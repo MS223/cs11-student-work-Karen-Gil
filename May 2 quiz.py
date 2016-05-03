68a050fb0934aa32c6cc79a972258ec775988296
Karen-Gil <Karen.gil@ms223.org>
2016-05-03 09:21:06 -0400

    Update May 2 quiz.py

--- May 2 quiz.py
+++ May 2 quiz.py
@@ -1,11 +1,24 @@
-value= input("pick a number")
-max_value=range(1,value +1)
-print(max_value)
+def max_value(integer):
+    print range(1,integer +1)
+max_value(10)
 
 list1 = [4,5,15,11,23,42]
 list2 = [1,8,7,16,7,35]
-print 
-def compare_lists(lists1,lists2)
-for x in list1:
-    if x >list2list1
-    
+print
+def compare_lists():
+    for x in list1:
+     if x > list2[list1.index(x)]:
+        print x
+     else:
+        print list2[list1.index(x)]
+compare_lists()
+
+def swapping_star():
+    width= input("How wide")
+    height= input("How tall")
+    for x in range(0,height):
+        if x % 2 == 1:
+            print (width/2)* '*-'
+        else:
+            print(width/2)* "-*"
+swapping_star()
