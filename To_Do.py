21cda4cbd5b046f72987cb18ff582bc699b67da4
Karen-Gil <Karen.gil@ms223.org>
2016-05-20 08:44:18 -0400

    Create  To_Do.py

---
+++ To_Do.py
@@ -0,0 +1,15 @@
+to_do= raw_input("What do you need to do")
+day=raw_input("What day").lower()
+days_of_week={
+    "Monday": [],
+    "Tuesday": [],
+    "Wednesday":[],
+    "Thursday": [],
+    "Friday": [],
+    "Saturday": [],
+    "Sunday": [],
+}
+def add():
+    days_of_week["day"] = to_do .append()
+print days_of_week
+# trying to get the to_do into the days of the weeks 
