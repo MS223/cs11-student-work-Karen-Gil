4229556c179a5ad1cb75c12491b15cf8d25e434b
Karen-Gil <Karen.gil@ms223.org>
2016-05-27 08:43:43 -0400

    Create Song_Lyrics.py

---
+++ Song_Lyrics.py
@@ -0,0 +1,26 @@
+class Song(object):
+
+    def __init__(self, lyrics,title,artist):
+        self.lyrics = lyrics
+        self.artist= title
+        self.title= artist
+
+    def sing_me_a_song(self,lyrics,title,artist):
+        print self.title
+        print self.artist
+        for line in self.lyrics:
+            print line
+
+
+happy_bday = Song(["Happy birthday to you",
+                   "These lyrics are copyrighted",
+                   "So I'll stop here"],"Happy Birthday","Who Knows")
+
+
+hotline_bling = Song(["You used to call me on my",
+                      "You used to, you used to",
+                      "Yeah",],"Hotline Bling", "Drake")
+# This is your daily reminder that Drake is Canadian
+
+happy_bday.sing_me_a_song()
+hotline_bling.sing_me_a_song()
